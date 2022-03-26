a2d3a4ea2335c18b3a80f03bc8b2cbda0cc96549
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-03-26 16:58:33 -0500

    reduced variable scope

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,31 @@
-const projects = [
-    {
-        name: "dately",
-        repo: "https://github.com/GitGals/Dately",
-        live: "https://gitgals.github.io/Dately/",
-    },
-    {
-        name: "weather",
-        repo: "https://github.com/freyaliesel/Weather-Dashboard",
-        live: "https://freyaliesel.github.io/Weather-Dashboard/",
-    },
-    {
-        name: "scheduler",
-        repo: "https://github.com/freyaliesel/Work-Day-Scheduler",
-        live: "https://freyaliesel.github.io/Work-Day-Scheduler/",
-    },
-    {
-        name: "quiz",
-        repo: "https://github.com/freyaliesel/Code-Quiz",
-        live: "https://freyaliesel.github.io/Code-Quiz/",
-    },
-    {
-        name: "scratch",
-        repo: "https://scratch.mit.edu/projects/497157717/editor/",
-        live: "https://scratch.mit.edu/projects/497157717/",
-    },
-];
-
 function redirect(target) {
+    const projects = [
+        {
+            name: "dately",
+            repo: "https://github.com/GitGals/Dately",
+            live: "https://gitgals.github.io/Dately/",
+        },
+        {
+            name: "weather",
+            repo: "https://github.com/freyaliesel/Weather-Dashboard",
+            live: "https://freyaliesel.github.io/Weather-Dashboard/",
+        },
+        {
+            name: "scheduler",
+            repo: "https://github.com/freyaliesel/Work-Day-Scheduler",
+            live: "https://freyaliesel.github.io/Work-Day-Scheduler/",
+        },
+        {
+            name: "quiz",
+            repo: "https://github.com/freyaliesel/Code-Quiz",
+            live: "https://freyaliesel.github.io/Code-Quiz/",
+        },
+        {
+            name: "scratch",
+            repo: "https://scratch.mit.edu/projects/497157717/editor/",
+            live: "https://scratch.mit.edu/projects/497157717/",
+        },
+    ];
     if (target.className.includes("content")) {
         let parent = target.closest("a");
         for (const project of projects) {
